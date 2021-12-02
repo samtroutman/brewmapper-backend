e92c6880d9c332158a15b1122714df00137854d3
samtroutman <troutm16@gmail.com>
2021-12-01 23:34:27 -0500

    fix case in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
 
-    def login
+    def create
         user = User.find_by_username(params[:username])
         if
             user && user.authenticate(params[:password]) 
-            render json: User
+            render json: user
         else
             render json: {errors: "Invalid username and/or password"}, status: :forbidden
         end
     end
+
 end
