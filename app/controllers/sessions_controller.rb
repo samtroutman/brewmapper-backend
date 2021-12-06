ba1dbd906abfb7eb71f8c5e72ab021688322c856
samtroutman <troutm16@gmail.com>
2021-12-05 20:24:48 -0500

    update autologin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     end
 
     def autologin
-        byebug
+        render json: {user: UserSerializer.new(logged_in_user), token: encode_token(logged_in_user.id)}
     end
 
 end
