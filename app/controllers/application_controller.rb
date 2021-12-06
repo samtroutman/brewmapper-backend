a492630d306be1e37e768fc3e248621b2d2d8293
samtroutman <troutm16@gmail.com>
2021-12-05 20:16:40 -0500

    add token methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::API
     def encode_token(user_id)
         JWT.encode({user_id: user_id}, ENV["JWT_SECRET"])
     end
+
+    def logged_in_user
+        User.find(decoded_token["user_id"])
+    end
+
+    private
+
+    def get_token
+        request.headers["Authorization"] 
+    end
+
+    def decoded_token
+        JWT.decode(get_token, ENV["JWT_SECRET"])[0]
+    end
 end
