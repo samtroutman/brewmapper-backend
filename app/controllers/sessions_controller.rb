8ae97b64b74e433750491c8ce38f28a1cb834c53
samtroutman <troutm16@gmail.com>
2021-12-05 19:23:41 -0500

    add jwt encode token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         user = User.find_by_username(params[:username])
         if
             user && user.authenticate(params[:password]) 
-            render json: user
+            render json: {user: UserSerializer.new(user), token: encode_token(user.id)}
         else
             render json: {errors: "Invalid username and/or password"}, status: :forbidden
         end
